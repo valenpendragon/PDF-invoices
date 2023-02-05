41bf226f3eb4e1fad4e3d8326d6354798ae4e0e8
Jeffrey Scott <valenpendragon@gmail.com>
2023-02-05 18:47:05 -0500

    Load the data into dataframes.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Purpose of This Project
 
 This is an app that creates PDF invoices from Excel spreadsheets.
+
+The current version will not have a GUI associated with it.
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import glob
+import os
+import pathlib
 
+filepaths = glob.glob("Invoices/*.xlsx")
+
+dataframes = []
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    dataframes.append(df)
+
+print(dataframes)
