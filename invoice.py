43eb96461ad674b99b9ee7f1e0eab82def46df15
Jeffrey Scott <valenpendragon@gmail.com>
2023-08-02 04:55:11 -0400

    Add optional variable for currency type with a default of USD.

--- invoice.py
+++ invoice.py
@@ -9,17 +9,19 @@ def generate(invoices_path, pdfs_path,
              product_name="product_name",
              amount_purchased="amount_purchased",
              price_per_unit="price_per_unit",
-             total_price="total_price"):
+             total_price="total_price",
+             currency_type="USD"):
     """
     This function requires the directory path for the spreadsheet invoices and the destination
     folder into which the function will print the PDF invoices. The remaining arguments are
     optional values for the columns and content in the printed PDFs. product_id, product_name,
     amount_purchase, and price_per_unit must match the corresponding column headings in the
-    spreadsheet invoices or the wrong data will be extracted. There are default values for all
+    spreadsheet invoices or the wrong data will be extracted. currency_type is used to set the
+    type of currency that will appear in the final PDF. There are default values for all
     of these strings, as they are needed for headings and PDF content.
 
-    Before these headers are used in the final PDF, underscores are converted to spaces, the
-    str.title method is applied, and id is converted to ID if it appears in a string.
+    Before column headers are used in the final PDF, underscores are converted to spaces, the
+    str.title() method is applied, and 'id' is converted to 'ID' if it appears in a string.
     Arguments:
     :param invoices_path: str, directory path
     :param pdfs_path: str, directory path
@@ -28,7 +30,8 @@ def generate(invoices_path, pdfs_path,
     :param amount_purchased: str, defaults to "amount_purchased"
     :param price_per_unit: str, defaults to "price_per_unit"
     :param total_price: str, defaults to "total_price"
-    :return:
+    :param currency_type: str, defaults to "USD" for US dollars
+    :return: None
     """
     filepaths = glob.glob(f"{invoices_path}/*.xlsx")
 
@@ -83,7 +86,7 @@ def generate(invoices_path, pdfs_path,
         pdf.ln(15)
         pdf.set_font(family="Times", size=10, style="B")
         pdf.set_text_color(80, 80, 80)
-        pdf.cell(w=30, h=8, txt=f"The total amount due is {invoice_total} Euros.", ln=1)
+        pdf.cell(w=30, h=8, txt=f"The total amount due is {invoice_total} {currency_type}.", ln=1)
         pdf.set_font(family="Times", size=10, style="B")
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=25, h=8, txt=f"ABC Company")
